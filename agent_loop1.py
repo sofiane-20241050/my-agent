55a41c1f5fd06a754c2240cf3e34c90b52e8dc9a
sofiane-20241050 <szz20241050@buaa.edu.cn>
2026-04-03 15:16:24 +0800

    modify agent_loop1.py

--- agent_loop1.py
+++ agent_loop1.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import json
+import inspect
 from typing import *
 from dataclasses import dataclass, field
 
@@ -25,20 +27,62 @@ class AgentConfig:
 
 # ===== Tool 基类 =====
 class Tool:
-    def __init__(self, name, description, schema):
-        self.name = name
-        self.description = description
-        self.schema = schema
-
+    name: str = None
+    description: str = None
+    parameters: Optional[dict] = None
+    
+    def get_schema(self) -> Dict:
+        """转换为 OpenAI function calling 格式"""
+        return {
+            "type": "function",
+            "function": {
+                "name": self.name,
+                "description": self.description,
+                "parameters": self.parameters,
+            },
+        }
     def exec(self, **kwargs):
         raise NotImplementedError("子类需要实现 exec 方法")
+    
+
+class ToolRegistry:
+    def __init__(self):
+        self.tools = {}  # mapping: name -> callable tool
+
+    def register(self, tool: Tool):
+        self.tools[tool.name] = tool
+
+    def get(self, name: str):
+        return self.tools.get(name)
+
+    def list(self):
+        return self.tools.values()
 
 
 class Add(Tool):
-    def __init__(self, name, description, schema):
-        super().__init__(name, description, schema)
-    def exec(self, **kwargs):
-        return super().exec(**kwargs)
+    name = "add"
+    description = "执行加法运算"
+    parameters = {
+        "type": "object",
+        "properties": {
+            "a": {
+                "type": "integer",
+                "description": "加数"
+            },
+            "b": {
+                "type": "integer",
+                "description": "被加数"
+            }
+    },
+        "required": ["a", "b"],
+    }
+
+    def exec(self, a: str, b: str):
+        # 将字符串转换为数字
+        a = float(a)  # 可以用 int(a) 或 float(a) 取决于你的需求
+        b = float(b)
+        result = a + b
+        return str(result)
 
 
 
@@ -52,3 +96,10 @@ def main():
         temperature=float(os.getenv("TEMPERATURE", 0.7)),
         max_tokens=int(os.getenv("MAX_TOKENS", 1024))
     )
+    add = Add()
+    schema = add.get_schema()
+    print(json.dumps(schema, indent=4, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    main()
